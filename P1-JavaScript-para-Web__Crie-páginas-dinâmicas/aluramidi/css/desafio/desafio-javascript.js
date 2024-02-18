75f2c5df015d0b4c3187ac2ad3d1486ee506a583
Fabricio santuchi <131098765+Fabricio-santuchi@users.noreply.github.com>
2024-02-18 19:11:41 -0300

    sv

---
+++ P1-JavaScript-para-Web__Crie-páginas-dinâmicas/aluramidi/css/desafio/desafio-javascript.js
@@ -0,0 +1,20 @@
+const lista = document.querySelectorAll('input[type=button]');
+const inputTel = document.querySelector('input[type=tel]');
+
+for(i=0;i<lista.length;i++){
+    
+    const tecla = lista[i];
+
+    tecla.onclick = function () {
+        inputTel.value += tecla.value
+    }
+
+    tecla.onkeydown = function (evento) {
+        if(evento.code === "Enter" || evento.code === "Space") {
+            tecla.classList.add('ativa');
+        }
+    }
+    tecla.onkeyup = function () {
+        tecla.classList.remove('ativa');
+    }
+}
