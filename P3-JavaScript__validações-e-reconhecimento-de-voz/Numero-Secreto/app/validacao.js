cf582e09b06d12a1982e2db6b7a7759b16c987df
Fabricio santuchi <131098765+Fabricio-santuchi@users.noreply.github.com>
2024-03-03 23:40:34 -0300

    sv

--- P3-JavaScript__validações-e-reconhecimento-de-voz/Numero-Secreto/app/validacao.js
+++ P3-JavaScript__validações-e-reconhecimento-de-voz/Numero-Secreto/app/validacao.js
@@ -2,7 +2,16 @@ function verificaSeOChutePossuiUmVaLorValido(chute){
     const numero = +chute
 
     if(chuteForInvalido(numero)){
-        elementoChute.innerHTML += '<div>valor Inválido!</div>';
+        if(chute.toUpperCase() === 'GAME OVER'){
+            document.body.innerHTML =`
+                <h2>Game Over!!!</h2>
+                <h3>Pressione o botão para jogar novamente</h3>
+                <button id="jogar-novamente" class="btn-jogar" >Jogar novamente</button>
+            `
+            document.body.style.backgroundColor = "black";
+        }else{
+            elementoChute.innerHTML += '<div>valor Inválido!</div>';
+        }
         return
     }
 
