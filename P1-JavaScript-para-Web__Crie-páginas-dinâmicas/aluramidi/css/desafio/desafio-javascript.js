ccd653f2e6579ad6f41d43face415343b220bf31
Fabricio santuchi <131098765+Fabricio-santuchi@users.noreply.github.com>
2024-09-27 10:24:52 -0300

    sv

--- P1-JavaScript-para-Web__Crie-páginas-dinâmicas/aluramidi/css/desafio/desafio-javascript.js
+++ P1-JavaScript-para-Web__Crie-páginas-dinâmicas/aluramidi/css/desafio/desafio-javascript.js
@@ -1,20 +1,19 @@
-const lista = document.querySelectorAll('input[type=button]');
-const inputTel = document.querySelector('input[type=tel]');
+const lista = document.querySelectorAll("input[type=button]");
+const inputTel = document.querySelector("input[type=tel]");
 
-for(i=0;i<lista.length;i++){
-    
-    const tecla = lista[i];
+for (i = 0; i < lista.length; i++) {
+  const tecla = lista[i];
 
-    tecla.onclick = function () {
-        inputTel.value += tecla.value
-    }
+  tecla.onclick = function () {
+    inputTel.value += tecla.value;
+  };
 
-    tecla.onkeydown = function (evento) {
-        if(evento.code === "Enter" || evento.code === "Space") {
-            tecla.classList.add('ativa');
-        }
+  tecla.onkeydown = function (evento) {
+    if (evento.code === "Enter" || evento.code === "Space") {
+      tecla.classList.add("ativa");
     }
-    tecla.onkeyup = function () {
-        tecla.classList.remove('ativa');
-    }
+  };
+  tecla.onkeyup = function () {
+    tecla.classList.remove("ativa");
+  };
 }
--- P1-JavaScript-para-Web__Crie-páginas-dinâmicas/aluramidi/main.js
+++ P1-JavaScript-para-Web__Crie-páginas-dinâmicas/aluramidi/main.js
@@ -1,46 +1,44 @@
-const listaDeTeclas = document.querySelectorAll('.tecla');
+const listaDeTeclas = document.querySelectorAll(".tecla");
 
 //Estrutura de repetição: colando function dentro das teclas.
-for(i = 0; i < listaDeTeclas.length; i++){
-    
-    const tecla = listaDeTeclas[i]; // pega as listas de teclas.
-    const instrumento = tecla.classList[1]; // pega o a class de todos.
-    const idAudio = `#som_${instrumento}`; // juntas as class o nome.
+for (i = 0; i < listaDeTeclas.length; i++) {
+  const tecla = listaDeTeclas[i]; // pega as listas de teclas.
+  const instrumento = tecla.classList[1]; // pega o a class de todos.
+  const idAudio = `#som_${instrumento}`; // juntas as class o nome.
 
-    listaDeTeclas[i].onclick = function () {
+  listaDeTeclas[i].onclick = function () {
     // adiciona todos os ids a uma function com parametro.
-        tocaSom(idAudio);
-    };
+    tocaSom(idAudio);
+  };
 
-    tecla.onkeydown = function(evento) {
-        if(evento.code === 'Space' || evento.code === 'Enter'){
-            tecla.classList.add('ativa')
-        }
+  tecla.onkeydown = function (evento) {
+    if (evento.code === "Space" || evento.code === "Enter") {
+      tecla.classList.add("ativa");
     }
-    tecla.onkeyup = function() {
-        tecla.classList.remove('ativa')
-    }
+  };
+  tecla.onkeyup = function () {
+    tecla.classList.remove("ativa");
+  };
 }
 
-function tocaSom(seletorAudio){
-    const som = document.querySelector(seletorAudio);
+function tocaSom(seletorAudio) {
+  const som = document.querySelector(seletorAudio);
 
-    if(som && som.localName === 'audio'){
-        pauseErepassa(som);
-    }else{
-        console.log('Elemento não encontrado ou seletor invalido!');
-    }
+  if (som && som.localName === "audio") {
+    pauseErepassa(som);
+  } else {
+    console.log("Elemento não encontrado ou seletor invalido!");
+  }
 }
 
 //-----------------------------------------------------
 //Pause o som que já está em reprodução e repete ele.
-function pauseErepassa(som){
-    if(som.paused){
-        som.play();
-    }else{
-        som.pause();
-        som.currentTime = 0;
-        som.play();
-    }
+function pauseErepassa(som) {
+  if (som.paused) {
+    som.play();
+  } else {
+    som.pause();
+    som.currentTime = 0;
+    som.play();
+  }
 }
-
